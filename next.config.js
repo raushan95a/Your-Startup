06c6343c76bb0554fce328a3541c64b3ae73ac93
Raushan Kumar <raushankr95a@gmail.com>
2026-04-19 19:13:38 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; connect-src 'self' https:;"
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
